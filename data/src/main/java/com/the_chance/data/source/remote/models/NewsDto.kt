07e9bfc50f70f3323c27806eec5d037b59df9d53
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-09-03 20:59:00 +0300

    create NewsDto model in remote data module

---
+++ data/src/main/java/com/the_chance/data/source/remote/models/NewsDto.kt
@@ -0,0 +1,45 @@
+package com.the_chance.data.source.remote.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsDto(
+    @SerializedName("data")
+    val `data`: List<Data?>?,
+    @SerializedName("pagination")
+    val pagination: Pagination?
+) {
+    data class Data(
+        @SerializedName("author")
+        val author: String?,
+        @SerializedName("category")
+        val category: String?,
+        @SerializedName("country")
+        val country: String?,
+        @SerializedName("description")
+        val description: String?,
+        @SerializedName("image")
+        val image: String?,
+        @SerializedName("language")
+        val language: String?,
+        @SerializedName("published_at")
+        val publishedAt: String?,
+        @SerializedName("source")
+        val source: String?,
+        @SerializedName("title")
+        val title: String?,
+        @SerializedName("url")
+        val url: String?
+    )
+
+    data class Pagination(
+        @SerializedName("count")
+        val count: Int?,
+        @SerializedName("limit")
+        val limit: Int?,
+        @SerializedName("offset")
+        val offset: Int?,
+        @SerializedName("total")
+        val total: Int?
+    )
+}
