5bffa1be4cfc4349db1e01ef6d40393a8c9bb560
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-09-16 12:17:39 +0300

    create login and signUp Screen & Route

--- app/src/main/java/com/the_chance/newswave/ui/features/login/LoginScreen.kt
+++ app/src/main/java/com/the_chance/newswave/ui/features/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.the_chance.newswave.ui.features.login
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,12 +13,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
@@ -26,6 +36,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.the_chance.newswave.R
 import com.the_chance.newswave.ui.components.AuthenticationTextField
 import com.the_chance.newswave.ui.components.AuthenticationTextFieldPassword
@@ -35,20 +46,30 @@ import com.the_chance.newswave.ui.components.Text
 import com.the_chance.newswave.ui.features.signup.SignupScreen
 import com.the_chance.newswave.ui.theme.space16
 import com.the_chance.newswave.ui.theme.space8
+import kotlinx.coroutines.launch
 import kotlin.math.sign
 
 @Composable
-fun LoginScreen(){
-    LoginContent()
+fun LoginScreen(
+    loginViewModel: LoginViewModel = hiltViewModel()
+){
+    val state by loginViewModel.loginState.collectAsState(initial = null)
+//    LoginContent(state = state, loginViewModel::loginUser)
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginContent(
+    state: LoginUiState?,
+    onClickLogin: (String, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     Box(
         modifier = Modifier
@@ -90,7 +111,7 @@ fun LoginContent(
             text = "",
             hint = stringResource(R.string.email),
             iconPainter = painterResource(id = R.drawable.ic_email),
-            onValueChange = { },
+            onValueChange = { email = it },
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Email,
                 imeAction = ImeAction.Next
@@ -98,14 +119,13 @@ fun LoginContent(
             keyboardActions = KeyboardActions(
                 onNext = { focusManager.moveFocus(FocusDirection.Next) }
             )
-            //                errorMessage = "state.emailState.errorState",
         )
 
         AuthenticationTextFieldPassword(
-            text = "",
+            text = "password",
             hint = stringResource(R.string.password),
             iconPainter = painterResource(id = R.drawable.ic_password),
-            onValueChange = { },
+            onValueChange = { password = it},
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Password,
                 imeAction = ImeAction.Next
@@ -113,7 +133,6 @@ fun LoginContent(
             keyboardActions = KeyboardActions(
                 onNext = { focusManager.moveFocus(FocusDirection.Next) }
             ),
-//                errorMessage = "state.passwordState.errorState",
         )
 
         FilledButton(
@@ -121,13 +140,35 @@ fun LoginContent(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = space8),
-            onClick = { },
+            onClick = { onClickLogin },
             isLoading = false
         )
+
+        if( state?.loading == true)
+            CircularProgressIndicator()
+
         SignupFooter(
             stringResource(R.string.you_don_t_have_account),
             stringResource(id = R.string.sign_up), modifier.padding(top = space8)
         )
+    }
+
+    LaunchedEffect(key1 = state?.isSuccess){
+        scope.launch {
+            if (state?.isSuccess?.isNotEmpty() == true){
+                val success = state.isSuccess
+                Toast.makeText(context, "$success", Toast.LENGTH_LONG)
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = state?.isError){
+        scope.launch {
+            if (state?.isError?.isNotEmpty() == true){
+                val error = state.isError
+                Toast.makeText(context, "$error", Toast.LENGTH_LONG)
+            }
+        }
     }
 }
 
--- app/src/main/java/com/the_chance/newswave/ui/features/signup/SignUpScreen.kt
+++ app/src/main/java/com/the_chance/newswave/ui/features/signup/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.the_chance.newswave.ui.features.signup
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,12 +13,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
@@ -26,6 +36,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.the_chance.newswave.R
 import com.the_chance.newswave.ui.components.AuthenticationTextField
 import com.the_chance.newswave.ui.components.AuthenticationTextFieldPassword
@@ -34,19 +45,31 @@ import com.the_chance.newswave.ui.components.SignupFooter
 import com.the_chance.newswave.ui.components.Text
 import com.the_chance.newswave.ui.theme.space16
 import com.the_chance.newswave.ui.theme.space8
+import kotlinx.coroutines.launch
 
 @Composable
-fun SignupScreen() {
-    SignupContent()
+fun SignupScreen(
+    signupViewModel: SignUpViewModel = hiltViewModel()
+) {
+    val state by signupViewModel.signupState.collectAsState(initial = null)
+
+//    SignupContent(state = state, signupViewModel::signupUser)
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SignupContent(
+    state: SignUpUiState?,
+    onClickSignup: (String, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
 
     Box(
         modifier = Modifier
@@ -96,15 +119,14 @@ fun SignupContent(
             keyboardActions = KeyboardActions(
                 onNext = { focusManager.moveFocus(FocusDirection.Next) }
             )
-            //                errorMessage = "error message",
         )
 
         AuthenticationTextField(
             oneLineOnly = true,
-            text = "",
+//            text = "Email",
             hint = stringResource(R.string.email),
             iconPainter = painterResource(id = R.drawable.ic_email),
-            onValueChange = { },
+            onValueChange = {email = it },
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Email,
                 imeAction = ImeAction.Next
@@ -112,14 +134,13 @@ fun SignupContent(
             keyboardActions = KeyboardActions(
                 onNext = { focusManager.moveFocus(FocusDirection.Next) }
             )
-            //                errorMessage = "state.emailState.errorState",
         )
 
         AuthenticationTextFieldPassword(
-            text = "",
+//            text = "Password",
             hint = stringResource(R.string.password),
             iconPainter = painterResource(id = R.drawable.ic_password),
-            onValueChange = { },
+            onValueChange = { password = it },
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Password,
                 imeAction = ImeAction.Next
@@ -127,7 +148,6 @@ fun SignupContent(
             keyboardActions = KeyboardActions(
                 onNext = { focusManager.moveFocus(FocusDirection.Next) }
             ),
-//                errorMessage = "state.passwordState.errorState",
         )
 
         AuthenticationTextFieldPassword(
@@ -138,7 +158,6 @@ fun SignupContent(
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
             keyboardActions = KeyboardActions(
                 onDone = { keyboardController?.hide() })
-            //                errorMessage = "state.confirmPasswordState.errorState",
         )
 
         FilledButton(
@@ -146,13 +165,36 @@ fun SignupContent(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = space8),
-            onClick = { },
+            onClick = { onClickSignup },
             isLoading = false
         )
+
+        if( state?.loading == true)
+            CircularProgressIndicator()
+
         SignupFooter(
             stringResource(id = R.string.already_have_account),
             stringResource(id = R.string.log_in)
         )
+
+    }
+
+    LaunchedEffect(key1 = state?.isSuccess){
+        scope.launch {
+            if (state?.isSuccess?.isNotEmpty() == true){
+                val success = state.isSuccess
+                Toast.makeText(context, "$success", Toast.LENGTH_LONG)
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = state?.isError){
+        scope.launch {
+            if (state?.isError?.isNotEmpty() == true){
+                val error = state.isError
+                Toast.makeText(context, "$error", Toast.LENGTH_LONG)
+            }
+        }
     }
 }
 
