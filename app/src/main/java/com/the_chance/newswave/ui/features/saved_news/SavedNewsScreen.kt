64833810ebf76e2831b69eec5eed8532210f9d40
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-09-16 12:18:59 +0300

    create newsDetails , SavedNews Screens And Route

---
+++ app/src/main/java/com/the_chance/newswave/ui/features/saved_news/SavedNewsScreen.kt
@@ -0,0 +1,17 @@
+package com.the_chance.newswave.ui.features.saved_news
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SavedNewsScreen(
+
+){
+    SavedNewsContent()
+}
+
+@Composable
+fun SavedNewsContent(
+
+){
+
+}
--- data/src/main/java/com/the_chance/data/repository/NewsRepositoryImpl.kt
+++ data/src/main/java/com/the_chance/data/repository/NewsRepositoryImpl.kt
@@ -15,16 +15,3 @@ class NewsRepositoryImpl @Inject constructor(
             ?: throw NotFoundException())
     }
 }
-
-//override suspend fun getAllNews(): List<NewsArticle> {
-//    val response = newsService.getNewsArticle()
-//    if (response.isSuccessful) {
-//        val data = response.body()?.data ?: emptyList()
-//        return data.map { it!!.toNewsArticleEntity()  }
-//    } else {
-//        throw NewsFetchException("hhhhh")
-//    }
-//}
-//}
-//
-//class NewsFetchException(message: String) : Exception(message)
