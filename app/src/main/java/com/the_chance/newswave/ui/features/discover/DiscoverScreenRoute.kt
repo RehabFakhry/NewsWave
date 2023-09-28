ab5fec298e47fbf7bccc5d51081c09db4c97f12e
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-09-28 14:57:29 +0300

    remove unused function in discoverRoute

--- app/src/main/java/com/the_chance/newswave/ui/features/discover/DiscoverScreenRoute.kt
+++ app/src/main/java/com/the_chance/newswave/ui/features/discover/DiscoverScreenRoute.kt
@@ -7,10 +7,6 @@ import com.the_chance.newswave.ui.navigation.Screen
 
 private val ROUTE = Screen.DiscoverScreen.route
 
-fun NavController.navigateToDiscoverScreen(){
-    navigate(ROUTE)
-}
-
 fun NavGraphBuilder.discoverRoute(){
     composable(ROUTE) { DiscoverScreen() }
 }
