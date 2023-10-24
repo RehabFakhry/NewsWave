9b8c0e0a06db1eee8d528a16b38319b054113c2d
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-24 23:55:52 +0300

    create an extension function to map country to its full name

--- ui/src/main/java/com/the_chance/ui/ui/features/discover/DiscoverUiState.kt
+++ ui/src/main/java/com/the_chance/ui/ui/features/discover/DiscoverUiState.kt
@@ -1,4 +1,4 @@
-package com.the_chance.newswave.ui.features.discover
+package com.the_chance.ui.ui.features.discover
 
 import com.the_chance.domain.model.NewsArticle
 import com.the_chance.domain.utill.ErrorHandler
@@ -12,7 +12,12 @@ data class DiscoverUiState(
     val news: List<NewsArticleUiState> = emptyList(),
     val newsItem: NewsArticleUiState = NewsArticleUiState(),
     val chipSelected: ChipSelectedState = ChipSelectedState.GENERAL
-)
+){
+    val nonDeprecatedNews = news
+        .distinctBy { it.title to it.image to it.country }
+        .filter { !it.author.isNullOrEmpty() && !it.image.isNullOrEmpty() && !it.country.isNullOrEmpty() }
+        .shuffled().take(5)
+}
 
 data class NewsArticleUiState(
     val author: String = "",
@@ -22,9 +27,7 @@ data class NewsArticleUiState(
     val source: String = "",
     val image: String = "",
     val category: String = "",
-    val language: String = "",
     val country: String = "",
-    val publishedAt: String = "",
 )
 
 fun NewsArticle.toNewUiState(): NewsArticleUiState {
@@ -36,9 +39,7 @@ fun NewsArticle.toNewUiState(): NewsArticleUiState {
         source = source ?: "",
         image = image ?: "",
         category = category ?: "",
-        language = language ?: "",
-        country = country ?: "",
-        publishedAt = publishedAt ?: "",
+        country = country?.convertCountryCodeToFullName() ?: "",
     )
 }
 
@@ -61,3 +62,30 @@ fun DiscoverUiState.sports() = this.chipSelected == ChipSelectedState.SPORTS
 fun DiscoverUiState.technology() = this.chipSelected == ChipSelectedState.TECHNOLOGY
 
 fun DiscoverUiState.showDiscover() = news.isNotEmpty()
+
+fun String.convertCountryCodeToFullName(): String {
+    val countryMap = mapOf(
+        "eg" to "Egypt",
+        "tr" to "Turkey",
+        "mo" to "Morocco",
+        "sa" to "Saudi Arabia",
+        "us" to "United States",
+        "uk" to "United Kingdom",
+        "ca" to "Canada",
+        "au" to "Australia",
+        "nz" to "New Zealand",
+        "ar" to "Argentina",
+        "br" to "Brazil",
+        "co" to "Colombia",
+        "de" to "Germany",
+        "in" to "India",
+        "bg" to "Bulgaria",
+        "it" to "Italy",
+        "fr" to "France",
+        "mx" to "Mexico",
+        "za" to "South Africa",
+    )
+
+    val countryCode = this.toLowerCase()
+    return countryMap[countryCode] ?: this // Return the original code if not found in the map
+}
