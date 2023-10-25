dbb36378d09689eb288613838f415f4337377bcb
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-25 18:11:09 +0300

    refactor useCase to get news in a range of time from 1 month ago till now

--- domain/src/main/java/com/the_chance/domain/usecase/GetAllNewsUseCase.kt
+++ domain/src/main/java/com/the_chance/domain/usecase/GetAllNewsUseCase.kt
@@ -2,12 +2,15 @@ package com.the_chance.domain.usecase
 
 import com.the_chance.domain.model.NewsArticle
 import com.the_chance.domain.repository.NewsRepository
+import java.time.LocalDate
 import javax.inject.Inject
 
 class GetAllNewsUseCase @Inject constructor(
     private val newsRepository: NewsRepository,
 ) {
-    suspend operator fun invoke(): List<NewsArticle> {
-        return newsRepository.getAllNews() ?: emptyList()
+    suspend operator fun invoke(currentDate: LocalDate): List<NewsArticle> {
+        val oneMonthAgo = currentDate.minusMonths(1)
+        val dateRange = "$oneMonthAgo,$currentDate"
+        return newsRepository.getAllNews(dateRange) ?: emptyList()
     }
 }
