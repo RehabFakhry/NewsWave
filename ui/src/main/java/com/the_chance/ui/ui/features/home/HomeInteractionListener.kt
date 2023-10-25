902aaa0d35d613acfc4fa884095dbc3c64df5a46
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-25 18:24:55 +0300

    delete duplicate functions and add function when click on card

--- ui/src/main/java/com/the_chance/ui/ui/features/home/HomeInteractionListener.kt
+++ ui/src/main/java/com/the_chance/ui/ui/features/home/HomeInteractionListener.kt
@@ -3,9 +3,7 @@ package com.the_chance.ui.ui.features.home
 interface HomeInteractionListener {
     fun getAllNewsArticle()
     fun getAllNewsByDate()
-    fun onClickBreakingNews()
+    fun onClickNewsCard()
     fun onClickSearchIcon()
     fun onClickShowMore()
-    fun onClickRecommendedNews()
-    fun onClickWorldNews()
 }
